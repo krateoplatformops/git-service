86d1f51f178f9054d134c73c333a3042329c7dfe
mauro sala <mauro.sala@gmail.com>
2022-05-25 18:02:03 +0200

    feat(controller): remove github actions

--- controllers/read.git.controller.js
+++ controllers/read.git.controller.js
@@ -62,31 +62,5 @@ router.get('/file/:url/:endpoint/:name', async (req, res, next) => {
   }
 })
 
-router.get('/pipeline/:url/:endpoint/:name', async (req, res, next) => {
-  try {
-    const parsed = uriHelpers.parse(stringHelpers.b64toAscii(req.params.url))
-
-    const endpoint = JSON.parse(stringHelpers.b64toAscii(req.params.endpoint))
-
-    logger.debug(endpoint)
-
-    switch (endpoint?.type) {
-      case 'github':
-        const content = await gitHubHelpers.readActionsByName(
-          endpoint,
-          parsed,
-          stringHelpers.b64toAscii(req.params.name)
-        )
-        res.status(200).json({
-          content: content
-        })
-        break
-      default:
-        throw new Error(`Unsupported endpoint ${parsed.domain}`)
-    }
-  } catch (error) {
-    next(error)
-  }
-})
 
 module.exports = router
